a0af1fddbbd8ace3874361817bb74fdd0579c319
krv1974 <63528758+krv1974@users.noreply.github.com>
2024-03-10 14:50:56 +1300

    asdasd

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var tableContainer = document.getElementById("table-container");
-    if (!tableContainer)
-        return; // Check if tableContainer exists
-    var tableHTML = "<table>";
-    // Adjust the number of rows and columns as per your requirement
-    var rows = 14;
-    var cols = 14;
-    // Generate the table
-    for (var i = 0; i < rows; i++) {
-        tableHTML += "<tr>";
-        for (var j = 0; j < cols; j++) {
-            tableHTML += "<td></td>";
-        }
-        tableHTML += "</tr>";
-    }
-    tableHTML += "</table>";
-    tableContainer.innerHTML = tableHTML;
-});
+
+
+const primeRow = [0, 2, 6, 5, 8, 7, 11, 9, 10, 4, 1, 3];
 var conversion = new Map([
-    ['0', 'A'],
-    ['1', 'B&#9839;'],
+    [0, 'A'],
+    [1, 'B&#9839;'],
+    [2, 'B'],
+    [3, 'C'],
+    [4, 'D&#9839;'],
+    [5, 'D'],
+    [6, 'E&#9839;'],
+    [7, 'E'],
+    [8, 'F'],
+    [9, 'G&#9839;'],
+    [10, 'G'],
+    [11, 'A&#9839;']
 ]);
-var x = conversion.keys();
+
+function retrograde(row) {
+    let retrograde = row.slice().reverse(); // Use slice() to create a copy before reversing
+    return retrograde;
+}
+function invert(row) {
+    let inverted = [];
+    row.forEach(element => {
+        inverted.push((12 - element) % 12);
+    });
+    return inverted;
+}
+function transpose(row, number) {
+    let transposed = [];
+    row.forEach(element => {
+        transposed.push((element + number) % 12);
+    });
+    return transposed;
+}
