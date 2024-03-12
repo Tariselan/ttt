4b37b7c8aa115259e22dbfe48d1d50057365770e
silly girl :3 <woodmoreaaron@gmail.com>
2024-03-12 18:01:07 +1300

    change

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
 const primeRow = [0, 2, 6, 5, 8, 7, 11, 9, 10, 4, 1, 3];
 var conversion = new Map([
     [0, 'A'],
-    [1, 'B&#9839;'],
+    [1, 'B&#9837;'],
     [2, 'B'],
     [3, 'C'],
-    [4, 'D&#9839;'],
+    [4, 'C&#9839;'],
     [5, 'D'],
-    [6, 'E&#9839;'],
+    [6, 'E&#9837;'],
     [7, 'E'],
     [8, 'F'],
-    [9, 'G&#9839;'],
+    [9, 'F&#9839;'],
     [10, 'G'],
-    [11, 'A&#9839;']
+    [11, 'G&#9839;']
 ]);
 
 function retrograde(row) {
@@ -34,3 +34,14 @@ function transpose(row, number) {
     });
     return transposed;
 }
+function rows() {
+    const rows = Array.from(document.getElementsByTagName('tr')).slice(1, 13);
+    rows.forEach(row => {
+        const children = Array.from(row.children).slice(1, 13);
+        children.forEach((child, index) => {
+            console.log(invert(primeRow, rows.indexOf(row))[rows.indexOf(row)]);
+            child.innerHTML = conversion.get(transpose(primeRow, invert(primeRow, rows.indexOf(row))[rows.indexOf(row)])[index]);
+        });
+    });
+}
+document.addEventListener('DOMContentLoaded', rows);
